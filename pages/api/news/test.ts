70b3707b2a019815c90457507345e31603ca54aa
josephsintum <sjumbam15@gmail.com>
2020-07-15 04:55:10 -0700

    reformatting code

--- pages/api/news/test.ts
+++ pages/api/news/test.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import natural from 'natural'
+import stopwords from '../../../utils/stopwords'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
     res.statusCode = 200
@@ -12,37 +13,31 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 
 async function news() {
     let response = await (await fetch('http://localhost:3000/api/news')).json()
-    response = response.news
-        .map((result) => {
-            // this merges the title and description
-            return result.title + ' ' + result.description
-        })
-        .join(' ')
     let tokenizer = new natural.WordTokenizer()
-    // tokenize takes texts and splits into words
-    let tokens = tokenizer.tokenize(response)
+    let tokens:string[] = response.news
+        .map((result: { title: string; description: string }) => {
+            return tokenizer.tokenize(result.title + ',' + result.description)
+        }).flat()
 
-    // cleaning up words
-    // removing numbers
-    tokens = tokens.filter((word) => {
-        return !Number(word)
-    })
-
-
-    // counts is an object of type {...{word: count}}
-    let counts = tokens.reduce((count, word) => {
+    /* 
+        todo: clean up
+        remove stop words 
+        remove fragments
+        stemming
+    */
+    let counts = tokens.reduce((count: { [key: string]: number }, word) => {
         word = word.toLowerCase()
-        count[word] = (count[word] || 0) + 1
+        if (isNaN(parseInt(word)) && !stopwords.map.has(word)) {
+            count[word] = (count[word] || 0) + 1
+        }
         return count
     }, {})
 
     // sorting object of words in ascending order
-    let sorted_counts = Object.keys(counts)
-        .sort((a, b) => counts[a] - counts[b])
-        .reduce((accumulator, v) => {
+    return Object.keys(counts)
+        .sort((a, b) => counts[b] - counts[a])
+        .reduce((accumulator: { [key: string]: number }, v) => {
             accumulator[v] = counts[v]
             return accumulator
         }, {})
-    console.log(sorted_counts)
-    return sorted_counts
 }
