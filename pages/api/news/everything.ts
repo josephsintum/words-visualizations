c62d7f84a86e986b333d4d4b86e99fc15458c83d
josephsintum <sjumbam15@gmail.com>
2020-07-31 17:44:56 -0700

    adding topheadlines url

--- pages/api/news/everything.ts
+++ pages/api/news/everything.ts
@@ -1,36 +1,61 @@
-import NewsAPI from 'newsapi'
+// GET response with all word database entries
+// POST adds word with frequency to database
+
+import nextConnect from 'next-connect'
 import { NextApiRequest, NextApiResponse } from 'next'
+// @ts-ignore
+import NewsAPI from 'newsapi'
+import middleware from '../../../middleware/middleware'
+import Article, { ArticleType } from '../../../models/article'
 
 const newsapi = new NewsAPI(process.env.API_KEY)
 
-export default (req: NextApiRequest, res:NextApiResponse) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
+const handler = nextConnect()
+handler.use(middleware)
 
-    news()
-        .then((response) => {
-            response = response.map((result) => result.articles)
-            response = response.flat()
-            res.end(JSON.stringify( response ))
-        })
-        .catch((error) => {
-            res.end(JSON.stringify(error))
-            console.error(error)
-        })
+interface MiddlewareRequest extends NextApiRequest {
+    body: ArticleType
 }
 
-async function news() {
-    let response = await newsapi.v2.sources({
-        language: 'en',
-        country: 'us',
-    })
-    let [...sources] = response.sources.map((source) => source.id)
-
-    let promises = sources.map((source) => {
-        return newsapi.v2.everything({
-            sources: source,
-            pageSize: 100,
-        })
-    })
-    return await Promise.all(promises)
+export interface NewsAPIResponse {
+    status: string
+    totalResults: number
+    articles: ArticleType[]
 }
+
+handler
+    .get(async (req: MiddlewareRequest, res: NextApiResponse) => {
+        res.json(await Article.find())
+    })
+    .post(async (req: MiddlewareRequest, res: NextApiResponse) => {
+        // fetching top headlines from newsAPI
+        let newsData: NewsAPIResponse = await newsapi.v2.topHeadlines({
+            language: 'en',
+        })
+
+        newsData.articles.map((article) => {
+            // checking if url already exist in database
+            Article.exists({ url: article.url }, (err, resBool) => {
+                // error handling
+                if (err) {
+                    console.log({ error: err })
+                    res.status(500).json({ error: err })
+                    return
+                }
+                // if article is unique, create in database
+                if (!resBool) {
+                    Article.create(article, (err) => {
+                        // error handling
+                        if (err) {
+                            console.log({ error: err })
+                            res.status(500).json({ error: err })
+                            return
+                        }
+                    })
+                }
+            })
+        })
+        res.json({ status: 'success' })
+    })
+
+export default handler
