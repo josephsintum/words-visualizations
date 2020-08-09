b89df1e4f2b3f1989907a517bf77cb6807a134de
josephsintum <sjumbam15@gmail.com>
2020-08-09 02:38:10 -0700

    adding query limits

--- pages/api/news/index.ts
+++ pages/api/news/index.ts
@@ -13,9 +13,36 @@ const newsapi = new NewsAPI(process.env.API_KEY)
 const handler = nextConnect()
 handler.use(middleware)
 
+interface IncomingRequest extends MiddlewareRequest {
+    query: {
+        // pageSize - returns number of news items per page
+        pageSize: string
+        // page - returns page (pagination)
+        page: string
+        // statSize - returns number of statistics per news item
+        statSize: string
+    }
+}
+
 handler
-    .get((req: MiddlewareRequest, res: NextApiResponse) => {
-        NewsModel.find()
+    .get((req: IncomingRequest, res: NextApiResponse) => {
+        /*
+         * Handles GET request .../api/news
+         * returns time series news statistics
+         * query param
+         *   pageSize: number - returns number of news items per page
+         *   page: number - returns page (pagination)
+         *   statSize: number - returns number of statistics per news item
+         */
+
+        let pageSize = Number(req.query.pageSize) || 20
+        let statSize = -(Number(req.query.statSize) || 25)
+        let page = Number(req.query.page) || 0
+
+        NewsModel.find({}, 'dateTime stats')
+            .slice('stats', statSize)
+            .limit(pageSize)
+            .skip(pageSize * page)
             .then((data) => {
                 res.json(data)
             })
@@ -25,8 +52,13 @@ handler
             })
     })
     .post(async (req: MiddlewareRequest, res: NextApiResponse) => {
+        /*
+         * Handles POST request .../api/news
+         * Makes a request to the news api for the top headlines
+         * updates database with the top headlines and news stats
+         * no params, body or header
+         */
         // fetching top headlines from newsAPI
-        // todo: handle error
         await newsapi.v2
             .topHeadlines({
                 language: 'en',
