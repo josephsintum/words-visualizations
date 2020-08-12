3d91c46763c0324fb5f95b74aabeb6858f2460f3
josephsintum <sjumbam15@gmail.com>
2020-08-12 02:05:26 -0700

    working demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,98 +1,44 @@
 import React, { useEffect, useState } from 'react'
-import { H1, H3 } from 'baseui/typography'
+import { Display4, Label1, H1, H3, H4 } from 'baseui/typography'
 import { Block } from 'baseui/block'
+import { SIZE, StyledSpinnerNext } from 'baseui/spinner'
 import {
     VictoryAxis,
     VictoryBar,
     VictoryBrushContainer,
     VictoryChart,
-    VictoryLine,
-    VictoryZoomContainer,
+    VictoryTheme,
 } from 'victory'
+
 import { NewsType } from '../models/news.model'
-import { Display4 } from 'baseui/typography'
-import { Spinner } from 'baseui/spinner'
 
-export const TestGraph = () => {
-    const [zoom, setZoom] = useState({})
-    const [selected, setSelected] = useState({})
+const calcWordFreq = (
+    statistics: NewsType[],
+    startTime: Date | number,
+    endTime: Date | number
+) => {
+    let start = new Date(startTime)
+    let end = new Date(endTime)
 
-    return (
-        <div>
-            <VictoryChart
-                width={800}
-                height={600}
-                scale={{ x: 'time' }}
-                containerComponent={
-                    <VictoryZoomContainer
-                        responsive={false}
-                        zoomDimension="x"
-                        zoomDomain={zoom}
-                        onZoomDomainChange={setSelected}
-                    />
-                }
-            >
-                <VictoryLine
-                    style={{
-                        data: { stroke: 'tomato' },
-                    }}
-                    data={[
-                        { x: new Date(1982, 1, 1), y: 125 },
-                        { x: new Date(1987, 1, 1), y: 257 },
-                        { x: new Date(1993, 1, 1), y: 345 },
-                        { x: new Date(1997, 1, 1), y: 515 },
-                        { x: new Date(2001, 1, 1), y: 132 },
-                        { x: new Date(2005, 1, 1), y: 305 },
-                        { x: new Date(2011, 1, 1), y: 270 },
-                        { x: new Date(2015, 1, 1), y: 470 },
-                    ]}
-                />
-            </VictoryChart>
-
-            <VictoryChart
-                width={800}
-                height={90}
-                scale={{ x: 'time' }}
-                padding={{ top: 0, left: 50, right: 50, bottom: 30 }}
-                containerComponent={
-                    <VictoryBrushContainer
-                        responsive={false}
-                        brushDimension="x"
-                        brushDomain={selected}
-                        onBrushDomainChange={setZoom}
-                    />
-                }
-            >
-                <VictoryAxis
-                    tickValues={[
-                        new Date(1985, 1, 1),
-                        new Date(1990, 1, 1),
-                        new Date(1995, 1, 1),
-                        new Date(2000, 1, 1),
-                        new Date(2005, 1, 1),
-                        new Date(2010, 1, 1),
-                        new Date(2015, 1, 1),
-                    ]}
-                    tickFormat={(x) => new Date(x).getFullYear()}
-                />
-                <VictoryLine
-                    style={{
-                        data: { stroke: 'tomato' },
-                    }}
-                    data={[
-                        { x: new Date(1982, 1, 1), y: 125 },
-                        { x: new Date(1987, 1, 1), y: 257 },
-                        { x: new Date(1993, 1, 1), y: 345 },
-                        { x: new Date(1997, 1, 1), y: 515 },
-                        { x: new Date(2001, 1, 1), y: 132 },
-                        { x: new Date(2005, 1, 1), y: 305 },
-                        { x: new Date(2011, 1, 1), y: 270 },
-                        { x: new Date(2015, 1, 1), y: 470 },
-                    ]}
-                />
-            </VictoryChart>
-        </div>
-    )
+    return statistics
+        .reduce((acc: NewsType['stats'][], stat) => {
+            let currTime = new Date(stat.dateTime)
+            if (start <= currTime && currTime <= end) acc.push(stat.stats)
+            return acc
+        }, [])
+        .flat()
+        .reduce((acc: NewsType['stats'], stat) => {
+            let count = acc.find((k) => k.word === stat.word)
+            if (count) {
+                ++count.frequency
+            } else {
+                acc.push({ word: stat.word, frequency: 1 })
+            }
+            return acc
+        }, [])
+        .sort((a, b) => {
+            return a.frequency - b.frequency
+        })
 }
 
 export default () => {
@@ -119,46 +65,104 @@ export default () => {
     return (
         <Block>
             <H1 color={'accent'}>Word Vis: Charting test</H1>
-            <Block width={'70%'}>
+            <Block width={'80%'}>
                 <H3>Time X frequency chart</H3>
                 <br />
+
                 {isLoaded.error ? (
                     <Display4 color={'negative'}>{isLoaded.error}</Display4>
                 ) : !isLoaded.isLoaded ? (
-                    <Spinner size={96} />
+                    <StyledSpinnerNext $size={SIZE.large} />
                 ) : (
-                    news.map((data) => (
-                        <Block key={`stat${data.dateTime.valueOf()}`}>
-                            <H3>{new Date(data.dateTime).toLocaleString()}</H3>
-                            <br />
-                            <VictoryChart
-                                horizontal
-                                domainPadding={25}
-                                height={400}
-                                padding={{ left: 80 }}
-                                // animate={{
-                                //     onLoad: {
-                                //         duration: 500,
-                                //     },
-                                // }}
-                            >
-                                <VictoryBar
-                                    data={data.stats.slice(-25)}
-                                    x="word"
-                                    y="frequency"
-                                    labels={({ datum }) => `${datum.frequency}`}
-                                />
-                            </VictoryChart>
-                            <br />
-                        </Block>
-                    ))
+                    <TestGraph news={news} />
                 )}
-                <br />
-                {/*<TestGraph news={news} />*/}
-                <br />
-
-                {}
             </Block>
         </Block>
+    )
+}
+
+export const TestGraph = ({ news }: { news: NewsType[] }) => {
+    const [selected, setSelected] = useState<{
+        x?: [Date, Date] | [number, number]
+    }>({ x: [new Date().setHours(6), new Date().setHours(12)] })
+    return (
+        <div>
+            {selected.x ? (
+                <Block>
+                    <VictoryChart
+                        theme={VictoryTheme.material}
+                        domainPadding={30}
+                        horizontal
+                        width={1000}
+                    >
+                        <VictoryBar
+                            x="word"
+                            y="frequency"
+                            data={calcWordFreq(
+                                news,
+                                new Date(selected.x[0]),
+                                new Date(selected.x[1])
+                            ).slice(-10)}
+                            labels={({ datum }) => `${datum.frequency}`}
+                        />
+                    </VictoryChart>
+                </Block>
+            ) : (
+                'pending'
+            )}
+            <br />
+            <H4 $style={{ textAlign: 'center' }}>
+                <Label1>
+                    {selected.x
+                        ? new Date(selected.x[0]).toLocaleDateString()
+                        : 'Date'}
+                </Label1>
+                {selected.x
+                    ? new Date(selected.x[0]).toLocaleTimeString()
+                    : 'Select'}
+                {' - '}
+                {selected.x
+                    ? new Date(selected.x[1]).toLocaleTimeString()
+                    : 'Time'}
+            </H4>
+            <br />
+            <VictoryChart
+                width={1000}
+                height={100}
+                scale={{ x: 'time' }}
+                padding={{ top: 0, left: 50, right: 50, bottom: 30 }}
+                containerComponent={
+                    <VictoryBrushContainer
+                        // responsive={false}
+                        brushDimension="x"
+                        brushDomain={selected}
+                        onBrushDomainChange={setSelected}
+                    />
+                }
+                theme={VictoryTheme.material}
+                domainPadding={30}
+            >
+                <VictoryAxis
+                    tickValues={Array.apply(null, Array(24)).map(
+                        (value, index) => {
+                            let t = new Date()
+                            t.setHours(index)
+                            return t
+                        }
+                    )}
+                    tickFormat={(x) => new Date(x).getHours()}
+                />
+                <VictoryBar
+                    data={Array.apply(null, Array(24)).map((value, index) => {
+                        let t = new Date()
+                        t.setHours(index)
+                        return {
+                            x: t,
+                            y: Math.floor(Math.random() * 10),
+                        }
+                    })}
+                />
+            </VictoryChart>
+        </div>
     )
 }
